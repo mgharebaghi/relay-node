616e5dc9108f70083a3d55e62630493b4942ba65
mohammad <mgharebaghi82@gmail.com>
2023-09-04 18:52:36 +0330

    change gossip

--- src/main.rs
+++ src/main.rs
@@ -81,9 +81,6 @@ async fn main() {
 
     let privacy = libp2p::gossipsub::MessageAuthenticity::Signed(keypair);
     let mut gossip_cfg_builder = libp2p::gossipsub::ConfigBuilder::default();
-    gossip_cfg_builder.heartbeat_interval(std::time::Duration::from_secs(10));
-    gossip_cfg_builder.mesh_n_high(20);
-    gossip_cfg_builder.gossip_lazy(20);
     gossip_cfg_builder.idle_timeout(Duration::from_secs(60 * 1000000));
     
     let gossip_cfg = libp2p::gossipsub::ConfigBuilder::build(&gossip_cfg_builder).unwrap();
