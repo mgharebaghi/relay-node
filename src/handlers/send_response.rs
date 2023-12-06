f544e4a23925eacff4193234db4c26aadfa369f9
mohammad <mgharebaghi82@gmail.com>
2023-12-06 14:55:59 +0330

    final commit

--- src/handlers/send_response.rs
+++ src/handlers/send_response.rs
@@ -3,7 +3,6 @@ use libp2p::{request_response::ResponseChannel, Swarm};
 use super::structures::{CustomBehav, Req, ReqForReq, Res, ResForReq};
 
 pub fn send_res(request: Req, swarm: &mut Swarm<CustomBehav>, channel: ResponseChannel<Res>) {
-    println!("{:?}", request);
     let response = Res {
         res: "You Are First Client".to_string(),
     };
@@ -20,5 +19,4 @@ pub fn send_res(request: Req, swarm: &mut Swarm<CustomBehav>, channel: ResponseC
             .send_response(channel, final_res)
             .unwrap();
     }
-    // let original_req: ReqForReq = serde_json::from_str(&request.req).unwrap();
 }
