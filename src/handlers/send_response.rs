9671c8d14f30082b399368b6e9a90b8e7d902bb0
mohammad <mgharebaghi82@gmail.com>
2023-12-06 14:33:41 +0330

    fix send response bug

--- src/handlers/send_response.rs
+++ src/handlers/send_response.rs
@@ -1,22 +1,24 @@
-use libp2p::{Swarm, request_response::ResponseChannel};
+use libp2p::{request_response::ResponseChannel, Swarm};
 
-use super::structures::{Req, CustomBehav, Res, ReqForReq, ResForReq};
+use super::structures::{CustomBehav, Req, ReqForReq, Res, ResForReq};
 
 pub fn send_res(request: Req, swarm: &mut Swarm<CustomBehav>, channel: ResponseChannel<Res>) {
     println!("{:?}", request);
     let response = Res {
         res: "You Are First Client".to_string(),
     };
-    let original_req: ReqForReq = serde_json::from_str(&request.req).unwrap();
-    let res = ResForReq {
-        peer: original_req.peer,
-        res: response,
-    };
-    let str_res = serde_json::to_string(&res).unwrap();
-    let final_res = Res { res: str_res };
-    swarm
-        .behaviour_mut()
-        .req_res
-        .send_response(channel, final_res)
-        .unwrap();
+    if let Ok(original_request) = serde_json::from_str(&request.req) {
+        let res = ResForReq {
+            peer: original_req.peer,
+            res: response,
+        };
+        let str_res = serde_json::to_string(&res).unwrap();
+        let final_res = Res { res: str_res };
+        swarm
+            .behaviour_mut()
+            .req_res
+            .send_response(channel, final_res)
+            .unwrap();
+    }
+    // let original_req: ReqForReq = serde_json::from_str(&request.req).unwrap();
 }
