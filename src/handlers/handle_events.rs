1ee5b30c3ef4c8b9760dae0f4ea89d887b3f069d
mohammad <mgharebaghi82@gmail.com>
2023-12-13 16:42:46 +0330

    chk bug2

--- src/handlers/handle_events.rs
+++ src/handlers/handle_events.rs
@@ -40,7 +40,7 @@ pub async fn events(
                 let ip: Ipv4Addr = ipv4.parse().unwrap();
                 if !ip.is_private() && ipv4 != "127.0.0.1" {
                     handle(address, local_peer_id, my_addresses).await;
-                    println!("address handled.");
+                    println!("listening start...");
                 } else {
                     execute!(
                         stdout(),
