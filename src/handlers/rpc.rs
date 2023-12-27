f3ca0cb91eb5def2c5f8ef728fe02187d6074d41
mohammad <mgharebaghi82@gmail.com>
2023-12-27 14:48:24 +0330

    handle req for utxo

--- src/handlers/rpc.rs
+++ src/handlers/rpc.rs
@@ -2,8 +2,9 @@ use libp2p::{
     futures::StreamExt,
     gossipsub::{Behaviour, IdentTopic},
     identity::Keypair,
+    request_response::{cbor, Config, Message, ProtocolSupport},
     swarm::SwarmEvent,
-    Multiaddr, SwarmBuilder,
+    Multiaddr, PeerId, StreamProtocol, SwarmBuilder,
 };
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
@@ -15,7 +16,7 @@ use std::{
 };
 use std::{net::SocketAddr, time::Duration};
 
-use axum::{extract, http::Method, routing::post, Router};
+use axum::{extract, http::Method, routing::post, Json, Router};
 use tower_http::cors::{AllowHeaders, Any, CorsLayer};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
@@ -88,6 +89,46 @@ pub struct OutputUnspent {
     #[serde_as(as = "DisplayFromStr")]
     pub unspent: Decimal,
     pub rnum: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct UTXO {
+    pub public_key: String,
+    pub utxos: Vec<UtxoData>,
+}
+
+pub async fn handle_requests() {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any)
+        .allow_headers(AllowHeaders::any());
+    let app: Router = Router::new()
+        .route("/tx", post(handle_transaction))
+        .route("/utxo", post(handle_utxo))
+        .layer(cors);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3390));
+
+    if let Some(ip) = public_ip::addr().await {
+        let full_address = format!("http://{}:3390", ip);
+        let client = reqwest::Client::new();
+        let res = client
+            .post("https://centichain.org/api/rpc")
+            .body(full_address)
+            .send()
+            .await;
+        match res {
+            Ok(_) => println!("Your address sent."),
+            Err(_) => println!("problem to send address!"),
+        }
+        println!("your public ip: {}", ip);
+    } else {
+        println!("You dont have public ip, listener: {}", addr);
+    }
+
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 async fn handle_transaction(extract::Json(transaction): extract::Json<Transaction>) -> String {
@@ -172,35 +213,128 @@ async fn handle_transaction(extract::Json(transaction): extract::Json<Transactio
     }
 }
 
-pub async fn handle_requests() {
-    let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST])
-        .allow_origin(Any)
-        .allow_headers(AllowHeaders::any());
-    let app: Router = Router::new()
-        .route("/rpc", post(handle_transaction))
-        .layer(cors);
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3390));
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Req {
+    pub req: String,
+}
 
-    if let Some(ip) = public_ip::addr().await {
-        let full_address = format!("http://{}:3390", ip);
-        let client = reqwest::Client::new();
-        let res = client
-            .post("https://centichain.org/api/rpc")
-            .body(full_address)
-            .send()
-            .await;
-        match res {
-            Ok(_) => println!("Your address sent."),
-            Err(_) => println!("problem to send address!"),
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Res {
+    pub res: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ResForReq {
+    pub peer: Vec<PeerId>,
+    pub res: Res,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ReqForReq {
+    peer: Vec<PeerId>,
+    req: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ReqForUtxo {
+    public_key: String,
+    request: String,
+}
+
+async fn handle_utxo(extract::Json(utxo_req): extract::Json<ReqForUtxo>) -> Json<UTXO> {
+    let keypair = Keypair::generate_ecdsa();
+    let peerid = PeerId::from(keypair.public());
+    let behaviour = cbor::Behaviour::<Req, Res>::new(
+        [(StreamProtocol::new("/mg/1.0"), ProtocolSupport::Full)],
+        Config::default(),
+    );
+
+    //config swarm
+    let swarm_config = libp2p::swarm::Config::with_tokio_executor()
+        .with_idle_connection_timeout(Duration::from_secs(10 * 60));
+    let mut swarm = SwarmBuilder::with_existing_identity(keypair)
+        .with_tokio()
+        .with_tcp(
+            Default::default(),
+            (libp2p::tls::Config::new, libp2p::noise::Config::new),
+            libp2p::yamux::Config::default,
+        )
+        .unwrap()
+        .with_quic()
+        .with_dns()
+        .unwrap()
+        .with_websocket(
+            (libp2p::tls::Config::new, libp2p::noise::Config::new),
+            libp2p::yamux::Config::default,
+        )
+        .await
+        .unwrap()
+        .with_behaviour(|_key| behaviour)
+        .unwrap()
+        .with_swarm_config(|_conf| swarm_config)
+        .build();
+
+    let listener: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse().unwrap();
+    swarm.listen_on(listener).unwrap();
+
+    let mut dial_addr = String::new();
+
+    let address_file = File::open("/etc/myaddress.dat").unwrap();
+    let reader = BufReader::new(address_file);
+    for i in reader.lines() {
+        let addr = i.unwrap();
+        if addr.trim().len() > 0 {
+            dial_addr.push_str(&addr);
+            break;
         }
-        println!("your public ip: {}", ip);
-    } else {
-        println!("You dont have public ip, listener: {}", addr);
     }
 
-    axum_server::bind(addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let dial_multiaddr: Multiaddr = dial_addr.parse().unwrap();
+    swarm.dial(dial_multiaddr).unwrap();
+
+    let request = ReqForReq {
+        peer: vec![peerid],
+        req: serde_json::to_string(&utxo_req).unwrap(),
+    };
+
+    loop {
+        match swarm.next().await.unwrap() {
+            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                let req = Req {
+                    req: serde_json::to_string(&request).unwrap(),
+                };
+                swarm.behaviour_mut().send_request(&peer_id, req);
+            }
+            SwarmEvent::Behaviour(req_res) => match req_res {
+                libp2p::request_response::Event::Message {  message, .. } => match message {
+                    Message::Response { response, .. } => {
+                        return handle_response(response);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+fn handle_response(response: Res) -> Json<UTXO> {
+    if let Ok(res) = serde_json::from_str::<ResForReq>(&response.res) {
+        if let Ok(utxo) = serde_json::from_str::<UTXO>(&res.res.res) {
+            return Json(utxo);
+        } else {
+            let utxo = UTXO {
+                public_key: "".to_string(),
+                utxos: Vec::new(),
+            };
+            return Json(utxo);
+        }
+    } else {
+        let utxo = UTXO {
+            public_key: "".to_string(),
+            utxos: Vec::new(),
+        };
+        return Json(utxo);
+    }
 }
