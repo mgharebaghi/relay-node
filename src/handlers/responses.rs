c24c689906198baaa68190b797628f5fdc35ed87
mohammad <mgharebaghi82@gmail.com>
2023-12-27 15:05:15 +0330

    handle req for utxo

--- src/handlers/responses.rs
+++ src/handlers/responses.rs
@@ -11,6 +11,7 @@ pub fn handle_responses(
     relay_topic_subscribers: &mut Vec<PeerId>,
     wallet_topic_subscribers: &mut Vec<PeerId>,
 ) {
+    println!("response: {:?}", response);
     let mut res: ResForReq = serde_json::from_str(&response.res).unwrap();
 
     if res.peer.last().unwrap() == &local_peer_id {
