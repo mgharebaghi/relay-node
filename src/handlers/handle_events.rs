e5836412701a8dc2b4993a9263144cc793f76dcd
mohammad <mgharebaghi82@gmail.com>
2024-01-01 19:06:41 +0330

    get all reciept

--- src/handlers/handle_events.rs
+++ src/handlers/handle_events.rs
@@ -75,6 +75,7 @@ pub async fn events(
                 break;
             }
             SwarmEvent::ConnectionClosed { peer_id, .. } => {
+                println!("conn closed!");
                 let index = client_topic_subscriber.iter().position(|c| *c == peer_id);
                 match index {
                     Some(i) => {
