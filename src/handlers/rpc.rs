f069116d18683a7bf2a3a4f8755473adeaf51f66
mohammad <mgharebaghi82@gmail.com>
2024-01-02 14:14:38 +0330

    block requests handle

--- src/handlers/rpc.rs
+++ src/handlers/rpc.rs
@@ -160,6 +160,17 @@ struct AllBlocksRes {
     all: Vec<Block>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockReq {
+    block_number: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct BlockRes {
+    block: Option<Block>,
+    status: String
+}
+
 pub async fn handle_requests() {
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
@@ -172,6 +183,7 @@ pub async fn handle_requests() {
         .route("/urec", post(handle_user_reciepts))
         .route("/allrec", get(handle_all_reciepts))
         .route("/allblocks", get(handle_all_blocks))
+        .route("/block", post(handle_block))
         .layer(cors);
     let addr = SocketAddr::from(([0, 0, 0, 0], 3390));
 
@@ -750,5 +762,107 @@ fn handle_allblocks_response(response: Res) -> Json<AllBlocksRes> {
     } else {
         let allblocks_res = AllBlocksRes { all: Vec::new() };
         return Json(allblocks_res);
+    }
+}
+
+async fn handle_block(extract::Json(block_req): extract::Json<BlockReq>) -> Json<BlockRes> {
+    let keypair = Keypair::generate_ecdsa();
+    let peerid = PeerId::from(keypair.public());
+    let behaviour = cbor::Behaviour::<Req, Res>::new(
+        [(StreamProtocol::new("/mg/1.0"), ProtocolSupport::Full)],
+        Config::default(),
+    );
+
+    //config swarm
+    let swarm_config = libp2p::swarm::Config::with_tokio_executor()
+        .with_idle_connection_timeout(Duration::from_secs(10 * 60));
+    let mut swarm = SwarmBuilder::with_existing_identity(keypair)
+        .with_tokio()
+        .with_tcp(
+            Default::default(),
+            (libp2p::tls::Config::new, libp2p::noise::Config::new),
+            libp2p::yamux::Config::default,
+        )
+        .unwrap()
+        .with_quic()
+        .with_dns()
+        .unwrap()
+        .with_websocket(
+            (libp2p::tls::Config::new, libp2p::noise::Config::new),
+            libp2p::yamux::Config::default,
+        )
+        .await
+        .unwrap()
+        .with_behaviour(|_key| behaviour)
+        .unwrap()
+        .with_swarm_config(|_conf| swarm_config)
+        .build();
+
+    let listener: Multiaddr = "/ip4/0.0.0.0/tcp/0".parse().unwrap();
+    swarm.listen_on(listener).unwrap();
+
+    let mut dial_addr = String::new();
+
+    let address_file = File::open("/etc/myaddress.dat").unwrap();
+    let reader = BufReader::new(address_file);
+    for i in reader.lines() {
+        let addr = i.unwrap();
+        if addr.trim().len() > 0 {
+            dial_addr.push_str(&addr);
+            break;
+        }
+    }
+
+    let dial_multiaddr: Multiaddr = dial_addr.parse().unwrap();
+    swarm.dial(dial_multiaddr).unwrap();
+
+    let request = ReqForReq {
+        peer: vec![peerid],
+        req: serde_json::to_string(&block_req).unwrap(),
+    };
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                let req = Req {
+                    req: serde_json::to_string(&request).unwrap(),
+                };
+                swarm.behaviour_mut().send_request(&peer_id, req);
+            }
+            SwarmEvent::Behaviour(req_res) => match req_res {
+                Event::Message { message, .. } => match message {
+                    Message::Response { response, .. } => {
+                        return handle_block_response(response);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+}
+
+fn handle_block_response(response: Res) -> Json<BlockRes> {
+    if let Ok(res) = serde_json::from_str::<ResForReq>(&response.res) {
+        if let Ok(block) = serde_json::from_str::<Block>(&res.res.res) {
+            let block_res = BlockRes {
+                block: Some(block),
+                status: "".to_string()
+            };
+            return Json(block_res);
+        } else {
+            let block_res = BlockRes {
+                block: None,
+                status: "Block not found!".to_string()
+            };
+            return Json(block_res);
+        }
+    } else {
+        let block_res = BlockRes {
+            block: None,
+            status: "Block not found!".to_string()
+        };
+        return Json(block_res);
     }
 }
