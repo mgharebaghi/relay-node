3e4937042813b4bf162a90bec1af26e01a9347bd
mohammad <mgharebaghi82@gmail.com>
2024-03-10 16:48:39 +0330

    get data from channel

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use relay_node::handle_requests;
 use relay_node::run;
 use relay_node::SWARM;
-use std::sync::Arc;
+// use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     let (_, _) = tokio::join!(
-        run(Arc::clone(&SWARM.0), SWARM.1),
+        run(SWARM.0.clone(), SWARM.1),
         handle_requests()
     );
 }
