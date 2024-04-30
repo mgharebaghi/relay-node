b6d547213f9417b3c43a572d6ffead359c5dce99
mohammad <mgharebaghi82@gmail.com>
2024-04-30 15:58:58 +0330

    change dev pages and scans

--- src/rpc/transaction.rs
+++ src/rpc/transaction.rs
@@ -1,18 +1,22 @@
+use std::str::FromStr;
+
 use axum::{
     extract::{self},
     Json,
 };
+use chrono::{SubsecRound, Utc};
 use mongodb::{
     bson::{to_document, Document},
     Collection,
 };
+use rust_decimal::Decimal;
 
 use crate::handlers::{db_connection::blockchain_db, structures::Transaction};
 
 use super::server::TxRes;
 
 pub async fn handle_transaction(
-    extract::Json(transaction): extract::Json<Transaction>,
+    extract::Json(mut transaction): extract::Json<Transaction>,
 ) -> Json<TxRes> {
     let mut tx_res = TxRes {
         hash: transaction.tx_hash.clone(),
@@ -23,6 +27,11 @@ pub async fn handle_transaction(
     match blockchain_db().await {
         Ok(db) => {
             let trx_coll: Collection<Document> = db.collection("Transactions");
+            transaction.date.clear();
+            transaction.fee = transaction.value * Decimal::from_str("0.01").unwrap();
+            transaction
+                .date
+                .push_str(&Utc::now().round_subsecs(0).to_string());
             let trx_doc = to_document(&transaction).unwrap();
             trx_coll.insert_one(trx_doc, None).await.unwrap();
             tx_res.status = "success".to_string();
