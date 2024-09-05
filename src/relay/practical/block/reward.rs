bebb0a464558a6480cc3d22e853aac28988948e3
mohammad <mgharebaghi82@gmail.com>
2024-09-05 20:23:18 +0330

    reward bug fixed

--- src/relay/practical/block/reward.rs
+++ src/relay/practical/block/reward.rs
@@ -8,12 +8,17 @@ pub struct Reward;
 impl Reward {
     pub fn calculate(last_block: &mut Vec<Block>) -> Decimal {
         let reward: Decimal;
-        if last_block[0].header.number % 1500000 == 0 {
-            reward =
-                last_block[0].body.coinbase.reward / Decimal::from_str("2.0").unwrap().round_dp(12);
-            reward
+        if last_block.len() > 0 {
+            if last_block[0].header.number % 1500000 == 0 {
+                reward = last_block[0].body.coinbase.reward
+                    / Decimal::from_str("2.0").unwrap().round_dp(12);
+                reward
+            } else {
+                reward = last_block[0].body.coinbase.reward;
+                reward
+            }
         } else {
-            reward = last_block[0].body.coinbase.reward;
+            reward = Decimal::from_str("50.0").unwrap();
             reward
         }
     }
